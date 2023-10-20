28c03e6784505252a7c3fad2db6fdb197045e705
Bogdan Grabinskij <admin@example.com>
2023-10-20 12:36:45 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='items-center'>
+      <div className="text-center w-full">
+        <button className="bg-emerald-400 p-2 px-4 rounded-md">Login</button>
+      </div>
+    </div>
+  )
+}
