3fd433bfaa4042ef00a3b2bb21f1b7ae9c375857
Bogdan Grabinskij <admin@example.com>
2023-10-20 20:06:17 +0200

    Add authentication with Google

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
 export default function Home() {
-  return (
-    <div className='items-center'>
-      <div className="text-center w-full">
-        <button className="bg-emerald-400 p-2 px-4 rounded-md">Login</button>
-      </div>
-    </div>
-  )
+    const { data: session } = useSession();
+    if(!session){
+        return (
+            <div className='items-center'>
+                <div className="text-center w-full">
+                    <button onClick={() => signIn('google')} className="bg-emerald-400 p-2 px-4 rounded-md">Login</button>
+                </div>
+            </div>)
+        }
+        return (
+            <div>Logged in {session.user.email}</div>
+        )
 }
